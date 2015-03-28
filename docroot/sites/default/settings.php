019ab9ac3c5cd1e0ab0f0b76fe210080116b82e8
Acquia Cloud <support@acquia.com>
2015-03-28 01:24:06 +0000

    Importing site archive

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -565,3 +565,12 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/pdca/pdca-settings.inc');
+}
